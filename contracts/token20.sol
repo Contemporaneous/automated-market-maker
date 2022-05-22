c47c1f27b408afbd3cc56b8552115fe1706ecb96
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-05-22 18:06:00 +1000

    Add token20.sol

---
+++ contracts/token20.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract LiquidityTokenA is ERC20 {
+    constructor(uint256 totalSupply) ERC20("TokenA", "TA") {
+        _mint(msg.sender, totalSupply);
+    }
+}
+
+contract LiquidityTokenB is ERC20 {
+    constructor(uint256 totalSupply) ERC20("TokenB", "TB") {
+        _mint(msg.sender, totalSupply);
+    }
+}
