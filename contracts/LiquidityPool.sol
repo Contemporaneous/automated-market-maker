b9f230177654fbb0d586cc1090c08e4ffdc26f5f
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-05-29 13:24:03 +1000

    Add transfer funcitonality for LiquidityPool

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -3,29 +3,82 @@ pragma solidity ^0.8.10;
 
 import "hardhat/console.sol";
 
-import '@oppenzepelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/interfaces/IERC20.sol';
 
 contract LiquidityPool {
     address public owner;
-    ERC20 public  tokenA;
-    ERC20 public  tokenB;
+    IERC20 public  tokenA;
+    IERC20 public  tokenB;
 
     uint256 private _totalSupply;
 
+    bool initialized;
 
-    constructor (ERC20 _tokenA, uint256 _balanceA, ERC20 _tokenB, uint256 _balanceB) payable {
+
+    constructor (IERC20 _tokenA, IERC20 _tokenB)  {
         owner = msg.sender;
 
-        require(_tokenA != address(0) && _tokenB != address(0));
+        require(address(_tokenA) != address(0) && address(_tokenB) != address(0));
         require(_tokenA != _tokenB);
-
-        _tokenA.transferFrom(msg.sender, address(this), _balanceA);
-        _tokenB.transferFrom(msg.sender, address(this), _balanceB);
 
         tokenA = _tokenA;
         tokenB = _tokenB;
 
-        _totalSupply = _balanceA * _balanceB;
-
+        initialized = false;
     }
+
+    function initialize(uint256 _tokenSupplyA, uint256 _tokenSupplyB) public {
+        require(tokenA.allowance(msg.sender, address(this)) >= _tokenSupplyA);
+        require(tokenB.allowance(msg.sender, address(this)) >= _tokenSupplyB);
+
+        tokenA.transferFrom(msg.sender, address(this), _tokenSupplyA);
+        tokenB.transferFrom(msg.sender, address(this), _tokenSupplyB);
+
+        _totalSupply = _tokenSupplyA * _tokenSupplyB;
+        initialized = true;
+    }
+
+    function gerBalanceA() public view returns (uint256) {
+        return tokenA.balanceOf(address(this));
+    }
+
+    function getBalanceB() public view returns (uint256) {
+        return tokenB.balanceOf(address(this));
+    }
+
+    function exactInTrafer(bool depositA, uint256 amount) public {
+
+        uint256 balanceA = tokenA.balanceOf(address(this));
+        uint256 balanceB = tokenB.balanceOf(address(this));
+
+        if (depositA) {
+            require(tokenA.allowance(msg.sender, address(this)) >= amount);
+            uint256 transfer = _totalSupply/(balanceA-amount) - balanceB;
+            tokenB.transfer(msg.sender, transfer);
+            tokenA.transferFrom(msg.sender, address(this), amount);
+        } else {
+            require(tokenB.allowance(msg.sender, address(this)) >= amount);
+            uint256 transfer = _totalSupply/(balanceB-amount) - balanceA;
+            tokenA.transferFrom(address(this), msg.sender, transfer);
+            tokenB.transfer(msg.sender, amount);
+        }
+    }
+
+    function exactOutTransfer(bool receiveA, uint256 amount) public {
+        uint256 balanceA = tokenA.balanceOf(address(this));
+        uint256 balanceB = tokenB.balanceOf(address(this));
+
+        if (receiveA) {
+            require(tokenA.allowance(msg.sender, address(this)) >= amount*2);
+            uint256 transfer = _totalSupply/(balanceA-amount) - balanceB;
+            tokenB.transferFrom(msg.sender, address(this), transfer);
+            tokenA.transferFrom(address(this), msg.sender, amount);
+        } else {
+            require(tokenB.allowance(msg.sender, address(this)) >= amount*2);
+            uint256 transfer = _totalSupply/(balanceB-amount) - balanceA;
+            tokenA.transferFrom(msg.sender, address(this), transfer);
+            tokenB.transferFrom(address(this), msg.sender, amount);
+        }
+    }
+
 }
